fd61f90ee8ab1985b6baf931065ff6ce6cafd95b
AmandineLabaeye <43751498+AmandineLabaeye@users.noreply.github.com>
2019-05-02 09:59:09 +0200

    Commenté le code javascript
    Prochain commit:
    Prioritaire:
    Système de mot de passe oublié à la connexion,
    Faire tout le CSS et les translations en même temps,
    Ajax pour les images du carroussel,
    Test Unitaire (JS et PHP)
    Formulaire de contact (Fini à tester lors de la mise en prod),
    
    Secondaire:
    Afficher leur propres publications / articles publié par eux sur leur profiles,
    Système de Liste d'amis,
    Notification a la personne qui recoit un commentaire

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -85,32 +85,48 @@ $("#Previous").click(function () {
 
 });
 
+// Fonction lors du clique sur le bouton j'aime
 function onClickBtnLike(event) {
+    // Pour qu'il est plus aucun evenement sur la page quand on arrive sur la fonction
     event.preventDefault();
 
+    // Constante qui contient url de la balise "a"
     const url = this.href;
+    // Constante qui contient la balise "span" qui à pour class "js-likes" en question
     const spanCount = this.querySelector('span.js-likes');
+    // Constante qui contient la balise "i" en question
     const icone = this.querySelector('i');
 
+    // Fonction Ajax sur l'url grâce à la constante
     axios.get(url).then(function (response) {
-        console.log(response);
+        // On insère une response data qui renvoie le nombre de likes dans la constante "spanCount"
         spanCount.innerHTML = response.data.likes;
 
+        // Condition qui signifis que si les balises "i", contiennent la class "fas", alors on remplace par
+        // "far", et dans le else ont fait l'inverse on remplace "far" par "fas"
         if (icone.classList.contains('fas')) {
             icone.classList.replace('fas', 'far');
         } else {
             icone.classList.replace('far', 'fas');
         }
+        // Les cas d'erreur par rapport au status
     }).catch(function (error) {
+        // Si la status d'erreur est égale à 403
         if (error.response.status === 403) {
+            // Alors ça renvoie une alerte comme quoi il faut vous connecté pour liker
             window.alert("Vous ne pouvez pas liker un article si vous n'êtes pas connecté!")
         } else {
+            // Else (Pour tout les autres cas d'erreur possible) ca renvoie une alerte comme quoi une erreur
+            // s'est produite et qu'il faut réessayer ultérieurement (Problème de connexion ou autres, 404)
             window.alert("Une erreur s'est produite, réessayer plus tard")
         }
 
     });
 }
 
+// La on selection grâce à la méthode natif toutes les balises "a" contenant la class "js-like-link",
+// on y applique une forEach dessus avec en paramètre "link"
 document.querySelectorAll('a.js-like-link').forEach(function (link) {
+    // Sur ce paramètre nous faisant on evement lorsque l'on clique qu'il exécute la fonction ci-dessus
     link.addEventListener('click', onClickBtnLike);
 });
