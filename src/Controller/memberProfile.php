a4c95285979aeadfe58124a9732e860b6ca01f6c
AmandineLabaeye <43751498+AmandineLabaeye@users.noreply.github.com>
2019-05-06 12:02:14 +0200

    Reste CSS côté Membre et Admin
    Ajout de la div "J'aime" sur la page des articles par catégories
    
    Prochain commit:
    Prioritaire:
    Faire tout le CSS,
    Test Unitaire (JS et PHP)
    Système de mot de passe oublié à la connexion (Fini à tester),
    Formulaire de contact (Fini à tester lors de la mise en prod),
    
    Secondaire: (pas le temps)
    Afficher leur propres publications / articles publié par eux sur leur profiles,
    Système de Liste d'amis,
    Notification a la personne qui recoit un commentaire

--- src/Controller/memberProfile.php
+++ src/Controller/memberProfile.php
@@ -18,7 +18,6 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
